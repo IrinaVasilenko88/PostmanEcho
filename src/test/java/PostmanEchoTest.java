46468bfb1bed2b7159d2c9c2e6c9bc0e551a11c7
Irina Vasilenko <vasilenko_i@yahoo.com>
2020-11-25 12:35:08 +0300

    test changed

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -8,7 +8,6 @@ public class PostmanEchoTest {
      void shouldShowBody(){
     given()
         .baseUri("https://postman-echo.com")
-        .contentType("text/plain; charset=UTF-8")
         .body("some data")
         .when()
         .post("/post")
